da5c3c676c571a49ed13de7b95e3ff2447da5712
HP <HP@DESKTOP-3E2CNLP>
2018-10-05 21:32:44 +0700

    Final Project

---
+++ src/cobaproyek1/FXMLLoginController.java
@@ -0,0 +1,31 @@
+package cobaproyek1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class FXMLLoginController {
+
+    @FXML
+    private Label lJudul;
+
+    @FXML
+    private Label lUser;
+
+    @FXML
+    private Label iPass;
+
+    @FXML
+    private TextField edituser;
+
+    @FXML
+    private TextField editpass;
+
+    @FXML
+    private Button btnOke;
+
+    @FXML
+    private Button btnCancel;
+
+}
